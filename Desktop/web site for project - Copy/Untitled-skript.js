613a01b794a231717856a29b731a08705001cc65
Konstatyn <sk.law.group@gmail.com>
2022-12-07 20:01:09 +0100

    1st comm

---
+++ Desktop/web site for project - Copy/Untitled-skript.js
@@ -0,0 +1,13 @@
+<script>
+    $('.btn-buy').click(function () {//клип на кнопку
+        var id = $(this).attr('id'); //получаем id этой кнопки
+            $.ajax({//передаем ajax-запросом данные
+            type: "POST", //метод передачи данных
+            url: '/addtocart.php',//php-файл для обработки данных
+            data: {id_tov: id},//передаем наши данные
+            success: function(data) {//
+                $('.basker_kol').html(data);//меняем количество товаров на кнопке корзины 
+                }
+            });
+    });
+</script>
